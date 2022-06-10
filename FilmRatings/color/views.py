74a800dca59f9cc5d663db010b626afa221c59d3
Maarten <mr.roos35@gmail.com>
2022-06-10 23:38:32 +0200

    Translate Django excercises to films and ratings
    * urls.py: Remove unused color URLs.
    
    * views.py: Comment out unused views, will be removed when no need
    appears to steal the code.
    
    * base_template.html: Namespace film_list URLs.
    
    * index.html:
    * details.html: Will be removed.
    
    * detail.html: Create template to display film ratings per film.
    
    * film_index.html: Create template to display films, with links to the
    rating details.
    
    * index.html: Namespace film_list URLs.
    Remove all color links except the color picker.
    
    * results.html: Create template to display film ratings result after
    committing a vote.

--- FilmRatings/color/views.py
+++ FilmRatings/color/views.py
@@ -2,10 +2,30 @@ from django.core.exceptions import ObjectDoesNotExist
 from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
+from django.views import generic
 
 from color.models import TableColor
 from color.forms.set_color import Color
 from FilmRatings import tools
+
+
+# class IndexView(generic.ListView):
+#     template_name = 'color/index.html'
+#     context_object_name = 'latest_question_list'
+#
+#     def get_queryset(self):
+#         """Return the last five published questions."""
+#         return TableColor.table_colors.order_by('-border_color')[:5]
+#
+#
+# class DetailView(generic.DetailView):
+#     model = TableColor
+#     template_name = 'color/details.html'
+#
+#
+# class ResultsView(generic.DetailView):
+#     model = TableColor
+#     template_name = 'color/results.html'
 
 
 # Color picker view.
@@ -41,77 +61,76 @@ def color(request):
     return render(request, 'color/color.html', context)
 
 
-def html_color_index(request):
-
-    # Preset parameters.
-    title = "Pure HTML colors index"
-    print(f'@@ in {title} debug. No action, no param.')
-
-    # Construct the context.
-    context = tools.add_base_context({
-        'title': title,
-    })
-    return render(request, 'color/html_color_index.html', context)
-
-
-def details(request, color_id):
-
-    # Initialize variables.
-    title = f'Color details {color_id}'
-    print(f'@@ in {title} debug. Action vote URL, param={color_id}.')
-    picked_color = get_object_or_404(TableColor, id=color_id)
-    title = f'Color {color_id} - {picked_color}'
-
-    # Process the form result.
-    try:
-        choice = request.POST['choice']
-        selected_color = TableColor.table_colors.get(id=choice)
-    except (KeyError, ObjectDoesNotExist):
-        # Redisplay the color voting form.
-        context = tools.add_base_context({
-            'title': title,
-            'picked_color': picked_color,
-        })
-        return render(request, 'color/details.html', context)
-    else:
-        selected_color.vote_count += 1
-        selected_color.save()
-        c = TableColor.table_colors.get(id=1)
-        c.border_color = selected_color.border_color
-        c.save()
-
-        # Always return an HttpResponseRedirect after successfully dealing
-        # with POST data. This prevents data from being posted twice if a
-        # user hits the Back button.
-        return HttpResponseRedirect(reverse('color:results', args=(selected_color.id,)))
+# def index(request):
+#
+#     # Preset parameters.
+#     title = "Pure HTML colors index"
+#     print(f'@@ in {title} debug. No action, no param.')
+#
+#     # Construct the context.
+#     context = tools.add_base_context({
+#         'title': title,
+#     })
+#     return render(request, 'color/index.html', context)
 
 
-def vote(request, color_id):
-    title = f'Vote {color_id}'
-    print(f'@@ in {title} debug. Action vote URL, param={color_id}.')
-    picked_color = get_object_or_404(TableColor, id=color_id)
-    try:
-        choice = request.POST['choice']
-        selected_color = TableColor.table_colors.get(id=choice)
-    except (KeyError, ObjectDoesNotExist):
-        # Redisplay the color voting form.
-        context = tools.add_base_context({
-            'title': title,
-            'picked_color': picked_color,
-            'error_message': "You didn't select a choice, you fool.",
-            # 'colors': TableColor.table_colors.order_by('id'),
-        })
-        return render(request, 'color/vote.html', context)
-    else:
-        selected_color.vote_count += 1
-        selected_color.save()
-        c = TableColor.table_colors.get(id=1)
-        c.border_color = selected_color.border_color
-        c.save()
-        # Always return an HttpResponseRedirect after successfully dealing
-        # with POST data. This prevents data from being posted twice if a
-        # user hits the Back button.
-        return HttpResponseRedirect(reverse('color:results', args=(selected_color.id,)))
+# def details(request, color_id):
+#
+#     # Initialize variables.
+#     title = f'Color details {color_id}'
+#     print(f'@@ in {title} debug. Action vote URL, param={color_id}.')
+#     picked_color = get_object_or_404(TableColor, id=color_id)
+#     title = f'Color {color_id} - {picked_color}'
+#
+#     # Process the form result.
+#     try:
+#         choice = request.POST['choice']
+#         selected_color = TableColor.table_colors.get(id=choice)
+#     except (KeyError, ObjectDoesNotExist):
+#         # Redisplay the color voting form.
+#         context = tools.add_base_context({
+#             'title': title,
+#             'picked_color': picked_color,
+#         })
+#         return render(request, 'color/details.html', context)
+#     else:
+#         selected_color.vote_count += 1
+#         selected_color.save()
+#         c = TableColor.table_colors.get(id=1)
+#         c.border_color = selected_color.border_color
+#         c.save()
+#
+#         # Always return an HttpResponseRedirect after successfully dealing
+#         # with POST data. This prevents data from being posted twice if a
+#         # user hits the Back button.
+#         return HttpResponseRedirect(reverse('color:results', args=(selected_color.id,)))
+
+
+# def vote(request, color_id):
+#     title = f'Vote {color_id}'
+#     print(f'@@ in {title} debug. Action vote URL, param={color_id}.')
+#     picked_color = get_object_or_404(TableColor, id=color_id)
+#     try:
+#         choice = request.POST['choice']
+#         selected_color = TableColor.table_colors.get(id=choice)
+#     except (KeyError, ObjectDoesNotExist):
+#         # Redisplay the color voting form.
+#         context = tools.add_base_context({
+#             'title': title,
+#             'picked_color': picked_color,
+#             'error_message': "You didn't select a choice, you fool.",
+#         })
+#         return render(request, 'color/vote.html', context)
+#     else:
+#         selected_color.vote_count += 1
+#         selected_color.save()
+#         c = TableColor.table_colors.get(id=1)
+#         c.border_color = selected_color.border_color
+#         c.save()
+#         # Always return an HttpResponseRedirect after successfully dealing
+#         # with POST data. This prevents data from being posted twice if a
+#         # user hits the Back button.
+#         return HttpResponseRedirect(reverse('color:results', args=(selected_color.id,)))
 
     #
     # # Process the request.
@@ -132,16 +151,16 @@ def vote(request, color_id):
     #     return HttpResponseRedirect(reverse('hello', args=(border_color.id,)))
 
 
-def results(request, color_id):
-    title = 'Vote Result'
-    print(f'@@ in {title} debug. No action, param={color_id}.')
-    picked_color = get_object_or_404(TableColor, id=color_id)
-    context = tools.add_base_context({
-        'title': title,
-        'picked_color': picked_color,
-    })
-    return render(request, 'color/results.html', context)
+# def results(request, color_id):
+#     title = 'Vote Result'
+#     print(f'@@ in {title} debug. No action, param={color_id}.')
+#     picked_color = get_object_or_404(TableColor, id=color_id)
+#     context = tools.add_base_context({
+#         'title': title,
+#         'picked_color': picked_color,
+#     })
+#     return render(request, 'color/results.html', context)
 
 
-def thanks(request):
-    return HttpResponse("Thanks for picking a color. Much appreciated.")
+# def thanks(request):
+#     return HttpResponse("Thanks for picking a color. Much appreciated.")
